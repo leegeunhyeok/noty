8014368f01ab2d298846c71582bc7718e2ee6235
leegeunhyeok <dev.ghlee@gmail.com>
2019-11-07 21:58:43 +0900

    UPDATE: 데스크탑 사이즈의 로그인 화면 구현

--- src/client/src/views/Signin.vue
+++ src/client/src/views/Signin.vue
@@ -1,5 +1,21 @@
 <template>
-  <div class="sign-in"></div>
+  <div class="sign-in">
+    <div class="sign-in__title">Noty</div>
+    <div class="sign-in__form">
+      <div class="sign-in__form__control">
+        <input type="text" spellcheck="false" placeholder="ID">
+      </div>
+      <div class="sign-in__form__control">
+        <input type="password" spellcheck="false" placeholder="Password">
+      </div>
+      <div class="sign-in__form__control">
+        <button>Login</button>
+      </div>
+    </div>
+    <div class="sign-in__signup">
+      <router-link to="/signup">Sign up</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,7 +25,70 @@ export default {
 </script>
 
 <style lang="scss">
+@import '@/styles/common';
+
 .sign-in {
-  // TODO
+  @extend .wh-100;
+  text-align: center;
+
+  &__title {
+    display: inline-block;
+    margin-top: 100px;
+    color: $tint;
+    font-size: 5rem;
+    border-bottom: 8px solid $bgTertiary;
+  }
+
+  &__form {
+    width: 100%;
+    max-width: 400px;
+    margin: auto;
+    margin-top: 180px;
+
+    &__control {
+      width: 100%;
+      margin: 15px 0;
+
+      input {
+        outline: none;
+        padding: 10px 20px;
+        width: 100%;
+        border: none;
+        border-radius: 5px;
+        background-color: $bgTertiary;
+        font-size: 1.4rem;
+
+        &::placeholder {
+          color: $placeholder;
+        }
+      }
+
+      button {
+        cursor: pointer;
+        outline: none;
+        padding: 10px 20px;
+        width: 100%;
+        border: none;
+        border-radius: 5px;
+        color: $onTint;
+        background-color: $tint;
+        font-size: 1.4rem;
+        font-weight: bold;
+      }
+    }
+  }
+
+  &__signup {
+
+    a {
+      color: $bgPrimary;
+      font-weight: bold;
+      text-decoration: none;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
 }
 </style>
