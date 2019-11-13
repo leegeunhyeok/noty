07005066064cd8f165c32578c692049c81b4c0d9
leegeunhyeok <dev.ghlee@gmail.com>
2019-11-13 21:50:37 +0900

    UPDATE: 헤더 및 트랜지션 효과 추가

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <transition name="header" mode="out-in">
+      <Header :title="headerTitle" v-if="showHeader"/>
+    </transition>
     <transition name="fade" mode="out-in">
       <router-view/>
     </transition>
@@ -7,10 +10,29 @@
 </template>
 
 <script>
+import Header from '@/components/Header'
 import gql from 'graphql-tag'
 
 export default {
   name: 'app',
+  components: {
+    Header
+  },
+  data () {
+    return {
+      showHeader: false,
+      headerTitle: 'Header'
+    }
+  },
+  watch: {
+    '$route.name': {
+      handler () {
+        this.showHeader = !~this.$route.name.toLowerCase().indexOf('sign')
+      },
+      deep: true,
+      immediate: true
+    }
+  },
   async mounted () {
     const result = await this.$apollo.mutate({
       // Query
