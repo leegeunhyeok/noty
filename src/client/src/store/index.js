e44cd0aa128265cc50b959e222713994a96723fe
leegeunhyeok <dev.ghlee@gmail.com>
2019-11-21 22:03:32 +0900

    UPDATE: userId 스토어 추가

--- src/client/src/store/index.js
+++ src/client/src/store/index.js
@@ -5,14 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    token: ''
+    token: '',
+    userId: ''
   },
   mutations: {
-    LOGIN (state, token) {
+    LOGIN (state, { token, userId }) {
       state.token = token
+      state.userId = userId
     },
     LOGOUT (state) {
       state.token = ''
+      state.userId = ''
     }
   },
   actions: {
