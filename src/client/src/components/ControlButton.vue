cbe0fb6e5ff735a42f3dfa4f36111ca832dcfe3e
leegeunhyeok <dev.ghlee@gmail.com>
2019-11-21 22:25:08 +0900

    ADD: 중앙에 배치된 조작용 버튼 컴포넌트 추가

---
+++ src/client/src/components/ControlButton.vue
@@ -0,0 +1,38 @@
+<template>
+  <button class="control-button"
+    @click="this.$emit('click')"
+  >+</button>
+</template>
+
+<script>
+export default {
+  name: 'control-button'
+}
+</script>
+
+<style lang="scss">
+@import '@/styles/common';
+
+.control-button {
+  cursor: pointer;
+  position: fixed;
+  bottom: 25px;
+  left: 50%;
+  width: 50px;
+  height: 50px;
+  font-size: 30px;
+  font-weight: bold;
+  line-height: 50px;
+  border-radius: 50%;
+  border: none;
+  outline: none;
+  color: $onTint;
+  background-color: $tint;
+  -webkit-transform: translateX(-50%);
+          transform: translateX(-50%);
+
+  &:hover {
+    background-color: darken($tint, 10%);
+  }
+}
+</style>
