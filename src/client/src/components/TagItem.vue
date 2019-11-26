687eaace263ac5811c6c6034c869910dbb898a8f
leegeunhyeok <dev.ghlee@gmail.com>
2019-11-26 22:58:58 +0900

    UPDATE: Tag 뷰 및 항목 컴포넌트 추가

---
+++ src/client/src/components/TagItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tag-item"
+    :style="{ color }"
+  >
+    <span :style="{ backgroundColor: color }"></span>
+    {{ name }}
+    <div class="close">x</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tag-item',
+  props: {
+    name: String,
+    color: String
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/styles/common';
+
+.tag-item {
+  position: relative;
+  width: 100%;
+  height: 40px;
+  padding: 10px;
+  background-color: $bgSecondary;
+  margin-bottom: 20px;
+  border-radius: 10px;
+  text-align: center;
+  font-weight: bold;
+
+  span {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    display: block;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+  }
+
+  .close {
+    cursor: pointer;
+    position: absolute;
+    top: 10px;
+    right: 16px;
+    color: $fgTeriary;
+    font-weight: bold;
+  }
+}
+</style>
--- src/client/src/views/Tag.vue
+++ src/client/src/views/Tag.vue
@@ -1,10 +1,39 @@
 <template>
-  <div class="tag"></div>
+  <div class="tag">
+    <TagItem v-for="({ name, color }, i) in tags"
+      :name="name"
+      :color="color"
+      :key="i"
+    />
+  </div>
 </template>
 
 <script>
+import TagItem from '@/components/TagItem'
+
 export default {
-  name: 'tag'
+  name: 'tag',
+  components: {
+    TagItem
+  },
+  data () {
+    return {
+      tags: []
+    }
+  },
+  created () {
+    this.getTags()
+  },
+  methods: {
+    getTags () {
+      // TODO: GraphQL 요청
+      this.tags = [
+        { name: 'Red', color: 'tomato' },
+        { name: 'Yellow', color: '#ffdb58' },
+        { name: 'Blue', color: 'dodgerblue' }
+      ]
+    }
+  }
 }
 </script>
 
