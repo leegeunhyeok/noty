76c44a4701f23a4c9f4d4fb69fc67301d2c02b4c
leegeunhyeok <dev.ghlee@gmail.com>
2019-11-26 23:29:51 +0900

    UPDATE: Todo 뷰 업데이트 및 항목 컴포넌트 추가

---
+++ src/client/src/components/TodoItem.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="todo-item">
+    <span class="todo-item__checkbox"
+      :class="{ done }"
+    ></span>
+    {{ content }}
+    <div class="close"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-item',
+  props: {
+    initDone: Boolean,
+    index: Number,
+    content: String,
+    tag: Object
+  },
+  date () {
+    return {
+      done: false
+    }
+  },
+  created () {
+    this.done = this.initDone
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/styles/common';
+
+.todo-item {
+  position: relative;
+  width: 100%;
+  height: 40px;
+  padding: 10px;
+  background-color: $bgSecondary;
+  margin-bottom: 20px;
+  border-radius: 10px;
+  text-align: center;
+  font-weight: bold;
+
+  &__checkbox {
+    cursor: pointer;
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    display: block;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: $bgTertiary;
+
+    &:hover {
+      background-color: darken($bgTertiary, 10%);
+    }
+
+    &.done {
+      background-color: $tint;
+    }
+  }
+
+  span {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    display: block;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+  }
+
+  .close {
+    cursor: pointer;
+    position: absolute;
+    top: 10px;
+    right: 16px;
+    color: $fgTeriary;
+    font-weight: bold;
+
+    &::before {
+      content: 'x';
+    }
+  }
+}
+</style>
--- src/client/src/views/Todo.vue
+++ src/client/src/views/Todo.vue
@@ -1,10 +1,79 @@
 <template>
-  <div class="todo"></div>
+  <div class="todo">
+    <div class="todo__control">
+      <div class="todo__control__filter">
+        <div class="todo__control__filter__item active">All</div>
+        <div class="todo__control__filter__item">Active</div>
+        <div class="todo__control__filter__item">Done</div>
+      </div>
+      <div class="todo__control__tag">
+        <div class="todo__control__tag__selected">All tag</div>
+        <div class="todo__control__tag__dropdown">
+          <div class="dropdown__item active">Red</div>
+          <div class="dropdown__item">Yellow</div>
+          <div class="dropdown__item">Blue</div>
+        </div>
+      </div>
+    </div>
+    <div class="todo__items">
+      <TodoItem v-for="({ done, index, content, tag }) in todos"
+        :initDone="done"
+        :index="index"
+        :content="content"
+        :tag="tag"
+        :key="index"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import TodoItem from '@/components/TodoItem'
+
 export default {
-  name: 'todo'
+  name: 'todo',
+  components: {
+    TodoItem
+  },
+  data () {
+    return {
+      todos: []
+    }
+  },
+  created () {
+    this.getTodos()
+  },
+  methods: {
+    getTodos () {
+      // TODO: GraphQL 요청
+      this.todos = [
+        {
+          tag: {
+            color: 'tomato'
+          },
+          done: false,
+          index: 1,
+          content: 'TODO 1'
+        },
+        {
+          tag: {
+            color: '#ffdb58'
+          },
+          done: true,
+          index: 2,
+          content: 'TODO 2'
+        },
+        {
+          tag: {
+            color: 'dodgerblue'
+          },
+          done: true,
+          index: 3,
+          content: 'TODO 3'
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -13,5 +82,74 @@ export default {
 
 .todo {
   @include page;
+
+  &__control {
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    margin-bottom: 20px;
+
+    &__filter {
+      float: left;
+
+      &__item {
+        cursor: pointer;
+        color: $fgTeriary;
+        font-weight: bold;
+        font-size: 1rem;
+        float: left;
+        margin-right: 16px;
+
+        &:hover {
+          color: darken($fgTeriary, 10%);
+        }
+
+        &.active {
+          color: $tint;
+        }
+      }
+    }
+
+    &__tag {
+      position: relative;
+      float: right;
+
+      &__selected {
+        cursor: pointer;
+        color: $tint;
+        background-color: $bgSecondary;
+        font-weight: bold;
+        border: 1px solid $separator;
+        border-radius: 5px;
+        padding: 0 10px;
+      }
+
+      &__dropdown {
+        position: absolute;
+        top: 45px;
+        right: 0;
+        border: 1px solid $separator;
+        border-radius: 5px;
+        overflow: hidden;
+        z-index: 1;
+
+        .dropdown__item {
+          cursor: pointer;
+          width: 100px;
+          color: $fgSecondary;
+          background-color: $bgSecondary;
+          padding: 5px;
+
+          &:hover {
+            background-color: darken($bgSecondary, 10%);
+          }
+        }
+      }
+    }
+  }
+
+  &__items {
+    // EMPTY
+  }
 }
 </style>
