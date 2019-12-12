6f9bfa713d14d66f70e06ad91ce443b62cd48ce3
leegeunhyeok <dev.ghlee@gmail.com>
2019-12-12 22:26:45 +0900

    UPDATE: 태그 저장 기능 구현

--- src/client/src/views/Tag.vue
+++ src/client/src/views/Tag.vue
@@ -5,21 +5,40 @@
       :color="color"
       :key="id"
     />
+    <ControlButton @click="toggleTagInput"
+      :style="{ 'transform': showTag ?
+        'translateX(-50%) rotate(45deg)' : 'translateX(-50%) ' }"
+    />
+    <transition name="fade" mode="out-in">
+      <div class="tag__input" v-if="showTag">
+        <div class="tag__input__item">
+          <input v-model="tagName" placeholder="Name">
+        </div>
+        <div class="tag__input__item">
+          <input v-model="color" placeholder="Color">
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
 import TagItem from '@/components/TagItem'
+import ControlButton from '@/components/ControlButton'
 import gql from 'graphql-tag'
 
 export default {
   name: 'tag',
   components: {
-    TagItem
+    TagItem,
+    ControlButton
   },
   data () {
     return {
-      tags: []
+      tags: [],
+      showTag: false,
+      tagName: '',
+      color: ''
     }
   },
   created () {
@@ -39,6 +58,32 @@ export default {
       })
 
       this.tags = data.userTag
+    },
+    toggleTagInput () {
+      if (this.showTag && this.tagName && this.color) {
+        this.addTag(this.tagName, this.color)
+      }
+      this.tagName = ''
+      this.color = ''
+      this.showTag = !this.showTag
+    },
+    async addTag (name, color) {
+      const { data } = await this.$apollo.mutate({
+        // Query
+        mutation: gql`mutation ($name: String!, $color: String!) {
+          createTag(name: $name, color: $color) {
+            id,
+            name,
+            color
+          }
+        }`,
+        variables: {
+          name,
+          color
+        }
+      })
+
+      this.tags.unshift(data.createTag)
     }
   }
 }
@@ -49,5 +94,36 @@ export default {
 
 .tag {
   @include page;
+
+  &__input {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    max-width: 400px;
+    width: 80%;
+    padding: 1rem;
+    background-color: $bgSecondary;
+    box-shadow: 0 0 5px rgba(0, 0, 0, .3);
+    border-radius: 5px;
+    -webkit-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+
+    &__item {
+      input {
+        outline: none;
+        padding: 5px 10px;
+        margin: 10px 0;
+        width: 100%;
+        border: none;
+        border-radius: 5px;
+        background-color: $bgTertiary;
+        font-size: 1.4rem;
+
+        &::placeholder {
+          color: $placeholder;
+        }
+      }
+    }
+  }
 }
 </style>
